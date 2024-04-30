9c4b35e998a601d73a65d36d19d0d1317ddfc619
Jacques ROUSSEL <jacques.roussel@rouaje.com>
2024-04-30 10:04:54 +0200

    [GOOFYS] Add dockerfile

---
+++ dockerfiles/goofys/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID $BUCKET $MOUNT_DIR
